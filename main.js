5d3274a233b26e9ac55a7e8d710594a065c29e4a
riswanbasheer566-max <riswanbasheer566@gmail.com>
2025-11-02 17:20:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -145,8 +145,8 @@ const soraCommand = require('./commands/sora');
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
+global.channelLink = "https://chat.whatsapp.com/Ej8cVGNcUbMC5S62YTEdCA?mode=wwt";
+global.ytch = "TMXR BOT";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
@@ -155,7 +155,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'TMRX',
             serverMessageId: -1
         }
     }
