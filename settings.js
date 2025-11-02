15bd568095701a22ec6b2e6cb7953576f4e82b9f
riswanbasheer566-max <riswanbasheer566@gmail.com>
2025-11-02 17:13:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
-  packname: 'Rafix Bot',
+  packname: 'TMXR',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Rafix Bot",
   botOwner: 'RIXWAN QW', // Your name
   ownerNumber: '919526862271', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
